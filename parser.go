940faea98c5bcae011b28a250bdcebb2587987d3
Egor <yudintsevegor@gmail.com>
2019-03-01 00:52:41 +0300

    look through all table correctly, but didnt save anything

--- parser.go
+++ parser.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 )
 
+var re = regexp.MustCompile(`[a-zA-z]([0-9]+)`)
+
 func main() {
 	res, err := http.Get("http://ras.phys.msu.ru/table/4/1.html")
 	if err != nil {
@@ -34,11 +36,7 @@ func main() {
 	})
 
 	grpbegin := "ГРУППЫ >>"
-	grpend := "<< ГРУППЫ"
-
-	tditem := "tditem"
-	tdsmall := "tdsmall"
-	tdtime := "tdtime"
+	grpEnd := "<< ГРУППЫ"
 
 	var flag bool
 	groups := make(map[string]string)
@@ -52,13 +50,13 @@ func main() {
 		}
 
 		text := std.Text()
-		if flag && text != grpend {
+		if flag && text != grpEnd {
 			groups = parseGroups(text, groups)
 			groups[text] = ""
 		}
 		if text == grpbegin {
 			flag = true
-		} else if text == grpend {
+		} else if text == grpEnd {
 			flag = false
 		}
 		//		fmt.Println(text)
@@ -71,12 +69,16 @@ func main() {
 	quantity := len(groups)
 	//	fmt.Println(quantity)
 	var binaryColumns = make([]float64, quantity)
+//	var indexSlice = make([]int, quantity)
+	
 	var time string
 	var nextStr bool
 
-	re := regexp.MustCompile(`[a-zA-z]([0-9]+)`)
 	var ind int
-	
+	tditem := "tditem"
+	tdsmall := "tdsmall"
+	tdtime := "tdtime"
+
 	doc.Find("td").Each(func(i int, std *goquery.Selection) {
 		//		fmt.Println("TD")
 		text := std.Text()
@@ -89,47 +91,75 @@ func main() {
 					ind = 0
 				} else if time == "" {
 					time = text
+					nextStr = true
 				} else {
 					time = text
 					nextStr = true
 					ind = 0
 //					fmt.Println(time, binaryColumns)
-					for i := 0; i < quantity - 1; i++{
+					for i := 0; i < quantity; i++ {
 						binaryColumns[i] = 0
 					}
 				}
 			}
 
 			if strings.Contains(class, tditem) {
-				num := re.FindStringSubmatch(class)[1]
-				number, err := strconv.Atoi(num)
+				number, err := fromStringToInt(class)
 				if err != nil {
 					log.Fatal(err)
 				}
 //				fmt.Println(class, number, len(binaryColumns), quantity)
-				for i := ind; i < ind + number - 1; i++{
+				for i := ind; i < ind+number; i++ {
 					binaryColumns[i]++
 				}
 				ind = ind + number
 			} else if strings.Contains(class, tdsmall) {
-				num := re.FindStringSubmatch(class)[1]
-				number, err := strconv.Atoi(num)
+				number, err := fromStringToInt(class)
 				if err != nil {
 					log.Fatal(err)
 				}
+
 				if number != 0 {
-	//			fmt.Println(class, ind, number, len(binaryColumns), quantity)
-				for i := ind; i < ind + number; i++{
-					binaryColumns[i] += 0.5
-				}
-				
-				fmt.Println(ind, ind + number - 1, time, binaryColumns)
-				ind = ind + number
+//					fmt.Println(nextStr)
+					if !nextStr {
+//						for j := 0; j < number; j++{
+//							binaryColumns[indexSlice[j]] += 0.5
+//						}
+//						indexSlice = indexSlice[:number]
+						for j := 0; j < number; j++{
+							for i := 0; i < quantity; i++ {
+								if binaryColumns[i] == 0.5 {
+									binaryColumns[i] += 0.5
+									break
+								}
+							}
+						}
+					} else {
+//					fmt.Println(class, ind, number, len(binaryColumns), quantity)
+						k := 0
+						for i := ind; i < ind+number; i++ {
+//							fmt.Println(k)
+//							indexSlice[k] = i
+							if binaryColumns[i] == 0 {
+								binaryColumns[i] += 0.5
+							}
+							k++
+						}
+						ind = ind + number
+					}
 				}
 			}
+			fmt.Println(time, binaryColumns, class)
 		}
 		//		fmt.Println(text)
 	})
+}
+
+func fromStringToInt(class string) (int, error) {
+	num := re.FindStringSubmatch(class)[1]
+	number, err := strconv.Atoi(num)
+
+	return number, err
 }
 
 func parseGroups(text string, mapka map[string]string) map[string]string {
